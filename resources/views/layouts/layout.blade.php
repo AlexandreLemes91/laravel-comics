0d47f3e8f033a63a72090743bbc866909f0e73ea
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-06-09 10:04:18 +0200

    favicon

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="{{ asset('imgs/favicon.ico') }}" type="image/x-icon">
         <title>DC COMICS</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
